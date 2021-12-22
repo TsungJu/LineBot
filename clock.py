c5581c91232fc8b61befbd6fd5cad1bdd6493df9
TsungJu <e285171@gmail.com>
2021-12-22 15:59:21 +0800

    change clock time

--- clock.py
+++ clock.py
@@ -6,16 +6,16 @@ import urllib.request
 from app_core import line_bot_api
 
 sched = BlockingScheduler()
-'''
-@sched.scheduled_job('cron',day_of_week='mon-fri',minute='*/20')
+
+@sched.scheduled_job('cron',day_of_week='tue-fri',minute='*/20')
 def scheduled_job():
     url = "https://leonardlinebot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
 
     for key, value in conn.getheaders():
         print(key,value)
-'''
-@sched.scheduled_job('cron',day_of_week='tue-fri',hour=18)
+
+@sched.scheduled_job('cron',day_of_week='wed,fri',hour=18)
 def scheduled_get_off_work_notify():
     line_bot_api.push_message('Ueee79758d328394e6a1017520482dec6',TextSendMessage(text='Time to get off work !'))
 
